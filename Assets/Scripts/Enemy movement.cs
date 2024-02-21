a95c76377b6a635a1a65fbb2b445c205be7954c6
RyanG4321 <r.p.g.gaff@gmail.com>
2024-02-21 19:01:15 +0000

    Enemy movement(unfinished)
    I've added the enemy movement though its bugged

--- Assets/Scripts/Enemy movement.cs
+++ Assets/Scripts/Enemy movement.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemymovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float sightrange;
+    private bool playerInRange;
+
+    public void Awake()
     {
-        
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        playerInRange = Physics.CheckSphere(transform.position, sightrange, whatIsPlayer);
+
+        if (!playerInRange)
+        {
+            patrolling();
+        }
+
+        if (playerInRange)
+        {
+            chasing();
+        }
+
+    }
+
+    public void patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //WalkPoint reached
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    public void SearchWalkPoint()
+    {
+        //calculate random point in range
+        float RandomZ = Random.Range(-walkPointRange, walkPointRange);
+        float RandomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + RandomX, transform.position.y, transform.position.z + RandomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, whatIsGround))
+            walkPointSet = true;
+    }
+
+    public void chasing()
+    {
+        agent.SetDestination(player.position);
+        transform.LookAt(player);
     }
 }
